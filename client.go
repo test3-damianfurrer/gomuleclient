aec9d26a8f9d1fa71ca76fdd19855849da8fbc97
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-09 23:05:20 +0100

    add flag to set uuid

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"hex"
 	//"github.com/test3-damianfurrer/gomule/emule"
 	"github.com/test3-damianfurrer/gomuleclient/emule"
 )
@@ -11,6 +12,7 @@ var (
 	debug    bool
 	server   string
 	username string
+	uuid     string
 	port     int
 )
 
@@ -18,6 +20,7 @@ func init() {
 	flag.BoolVar(&debug, "d", false, "Debug")
 	flag.StringVar(&server, "h", "localhost", "Server address")
 	flag.StringVar(&username, "u", "gomuleuser", "Username")
+	flag.StringVar(&uuid, "x", "6aff9d13ba4f4b67af0cf6a514c4d499", "User UUID hex format")
 	flag.IntVar(&port, "p", 7111, "Server Port number")
 }
 
@@ -28,6 +31,9 @@ func main() {
 	
 	client := emule.NewClientConn(server, port, debug)
 	client.Username=username
+	uuid_b:=hex.DecodeString(uuid)
+	client.Uuid=uuid_b
+	//0x6a,0xff,0x9d,0x13,0xba,0x4f,0x4b,0x67,0xaf,0x0c,0xf6,0xa5,0x14,0xc4,0xd4,0x99) //client uuid this.Uuid
 	client.Connect()
 	defer client.Disconnect()
 }
