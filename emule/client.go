a43ad634eeb9e6be38df720708d10b7d7e0155cf
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-09 22:14:41 +0100

    add mistery tag

--- emule/client.go
+++ emule/client.go
@@ -99,6 +99,8 @@ func (this *Client) Connect() {
 	body = append(body,util.EncodeByteTagString(util.EncodeByteTagNameInt(0x1),"gomuleclientuser")...)
 	body = append(body,util.EncodeByteTagInt(util.EncodeByteTagNameInt(0x11),uint32(0x3C))...)
 	body = append(body,util.EncodeByteTagInt(util.EncodeByteTagNameInt(0x20),uint32(0b1100011101))...)
+	body = append(body,util.EncodeByteTagInt(util.EncodeByteTagNameInt(0xfb),util.ByteToUint32([]byte{128, 13, 4, 3}))...)
+	//body = append(body,util.EncodeByteTagInt(util.EncodeByteTagNameInt(0x20),)...)
 	
 	fmt.Println("Size body", len(body))
 	data := util.EncodeByteMsg(0xE3,0x01,body)
