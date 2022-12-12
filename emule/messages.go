de401a53c8e22c59c6e62086d630c5581383fc0d
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-12 09:59:09 +0100

    decompress

--- emule/messages.go
+++ emule/messages.go
@@ -10,35 +10,38 @@ func handleServerMsg(protocol byte,buf []byte,dc libdeflate.Decompressor){
     	//0xd4
 	switch protocol {
 		case 0xe3:
-			decodeE3(buf)
+			decodeE3(buf[0],buf[1:len(buf)])
 		case 0xd4:
-			fmt.Printf("DEBUG: maybe type 0x%x\n",buf[0])
-			blen, decompressed, err := dc.Decompress(buf[1:len(buf)], nil, 1)
-			if err != nil {
-				fmt.Println("ERROR: failed to decompress buffer",err)
-				return
-			}
-			fmt.Printf("DEBUG: decompressed type 0x%x\n",decompressed[0])
-			fmt.Println("DEBUG: decompressed length:",blen)
-			fmt.Println("DEBUG: decompressed",decompressed[0:30])
+			decodeD4(buf[0],buf[1:len(buf)],dc)
 		default:
 			fmt.Println("ERROR: only std 0xE3 protocol supported")
 	}
 }
 
-func decodeE3(buf []byte){
-	bufsize := len(buf)
-	switch buf[0] {
+func decodeD4(btype byte,buf []byte,dc libdeflate.Decompressor){
+	fmt.Printf("DEBUG: 0xd4 type 0x%x\n",btype)
+	blen, decompressed, err := dc.Decompress(buf, nil, 1)
+	if err != nil {
+		fmt.Println("ERROR: failed to decompress buffer",err)
+		return
+	}
+	fmt.Println("DEBUG: decompressed length:",blen)
+	fmt.Println("DEBUG: decompressed",decompressed[0:30])
+	decodeE3(btype,decompressed)
+}
+
+func decodeE3(btype byte,buf []byte){
+	switch btype {
 			case 0x38:
-				prcServerTextMsg(buf[1:bufsize])
+				prcServerTextMsg(buf)
 			case 0x40:
-				prcIdChange(buf[1:bufsize])
+				prcIdChange(buf)
 			case 0x34:
-				prcServerStatus(buf[1:bufsize])
+				prcServerStatus(buf)
 			case 0x41:
-				prcServerIdentification(buf[1:bufsize])
+				prcServerIdentification(buf)
             default:
-            	fmt.Printf("ERROR: Msg type 0x%x not supported\n",buf[0])
+            	fmt.Printf("ERROR: Msg type 0x%x not supported\n",btype)
         }
 }
 
