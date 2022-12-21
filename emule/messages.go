ba15e9b72b7e5321229a1f73623d3c24cc1ec8bb
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-21 13:04:28 +0100

    Update messages.go

--- emule/messages.go
+++ emule/messages.go
@@ -146,9 +146,10 @@ func prcOneSearchResult(pos int, buf []byte) (readb int, fname_b []byte, hash_b 
 							//	fmt.Println("Debug: unknown tag/value: ",buf[readb:readb+4])
 							//	readb+=4 
 							// 136 212 1 14 148 213 - one thing
-							case 52:
-								fmt.Println("Debug: unknown tag/value: ",buf[readb:readb+4])
-								readb+=4
+							//case 52:
+							//	fmt.Println("Debug: unknown tag/value: ",buf[readb:readb+4])
+							//	readb+=4
+							//136 212 52 4 148 213
 							default:
 						
 					///*
